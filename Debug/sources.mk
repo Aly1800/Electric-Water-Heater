612d34ecf9b52a04999c9d0c4315e2494e7113cc
Aly1800 <A.Khaled2174@nu.edu.eg>
2025-07-16 17:58:34 +0300

    Add files via upload

---
+++ Debug/sources.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+FLASH_IMAGE := 
+ELFS := 
+OBJS := 
+ASM_DEPS := 
+EEPROM_IMAGE := 
+AVRDUDEDUMMY := 
+S_DEPS := 
+SIZEDUMMY := 
+S_UPPER_DEPS := 
+LSS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+APP \
+HAL/Buttons \
+HAL/ElementControl \
+HAL/SSD \
+HAL/TempSensor \
+MCAL/ADC \
+MCAL/DIO \
+MCAL/EEPROM \
+MCAL/EXTI \
+MCAL/Timer \
+MCAL/UART \
+
